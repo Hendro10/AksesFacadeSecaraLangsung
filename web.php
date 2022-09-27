adc2b4b795d80038bf00532e4e55f3db75081f69
Hendro Gunawan <112385556+Hendro10@users.noreply.github.com>
2022-09-27 16:19:29 +0700

    Update web.php

--- web.php
+++ web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PageController;
 
-Route::get('/', [App\Http\Controllers\Admin\PageController::class,'index']);
-Route::get('/mahasiswa',[App\Http\Controllers\Admin\PageController::class,'tampil']);
 Route::get('/coba-facade',[PageController::class,'cobaFacade']);
